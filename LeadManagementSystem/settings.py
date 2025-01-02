2babe501fc3e5438bc48e0027cb211d88b475fcb
Nitin Sharma <nitinvasishtha16@gmail.com>
2025-01-02 20:08:28 +0530

    settings.py

--- LeadManagementSystem/settings.py
+++ LeadManagementSystem/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=300),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
